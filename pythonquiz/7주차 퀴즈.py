582fa643d7a646cfe3cb087537ad7e89b1aa8b85
hayouseung <hayouseong0@gmail.com>
2024-04-19 15:35:42 +0900

    change

--- pythonquiz/7주차 퀴즈.py
+++ pythonquiz/7주차 퀴즈.py
@@ -1,7 +1,7 @@
 numbers = [[1,2,3], [4,5,6], [7,8,9]]
 results = []
-for a in range(0,3):
-    for b in range(0,3):
-        if (numbers[a][b] % 2) == 0:
-            results.append(numbers[a][b])
+for a in numbers:
+    for b in a:
+        if b % 2 == 0:
+            results.append(b)
 print(results)
