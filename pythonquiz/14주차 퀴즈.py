1bf6b0e1601d06fb25b47689632e8ae856377005
hayouseung <hayouseong0@gmail.com>
2024-06-07 13:52:41 +0900

    14주차 퀴즈 입니다.

--- pythonquiz/14주차 퀴즈.py
+++ pythonquiz/14주차 퀴즈.py
@@ -2,8 +2,8 @@ def menu_choice():
     menu = ["냉면", "볶음밥", "피자", "짜장면"]
     while True:
         try:
-            a = input("메뉴 번호를 입력해 주세요: ")
-            return menu[int(a) - 1]
+            menu_number = input("메뉴 번호를 입력해 주세요: ")
+            return menu[int(menu_number) - 1]
         except ValueError:
             print("메뉴에 적힌 숫자만 입력 가능 합니다. 다시 입력해 주세요.")
         except IndexError:
